e77da99d74ed89095510f31621214051eda50377
TakhirovBizhan <bijantakhirov2004@mail.ru>
2024-11-03 21:50:31 +0300

    new: main page is appoximately done.

--- src/App/pages/MainPage/MainPage.tsx
+++ src/App/pages/MainPage/MainPage.tsx
@@ -4,11 +4,67 @@ import '../../../styles/styles.scss'
 import MultiDropdown, {Option} from '../../../components/MultiDropdown';
 import Input from '../../../components/Input';
 import Button from '../../../components/Button';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Loader from '../../../components/Loader';
+import Card from '../../../components/Card';
+
 
 export const MainPage = () => {
 
-  const [inputValue, setInputValue] = useState('');
+  interface ICategory {
+    id: number,
+    name: string,
+    image: string,
+    creationAt: Date,
+    updatedAt: Date
+  }
+
+  interface IData {
+    id: number,
+    title: string,
+    price: number,
+    description: string,
+    images: string[],
+    category: ICategory,
+    creationAt: string,
+    updatedAt: string
+
+  }
+
+  const [inputValue, setInputValue] = useState<string>('');
+  const [data, setData] = useState<IData[]>([]);
+ // const [currentPage, setCurrentPage] = useState<number>(1);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+      const result = await axios({
+        method: 'get',
+        url: 'https://api.escuelajs.co/api/v1/products'
+      });
+
+      setData(result.data.map((raw: IData)=> ({
+        id: raw.id,
+        title: raw.title, 
+        price: raw.price,
+        description: raw.description,
+        images: raw.images,
+        category: raw.category,
+        creationAt: raw.creationAt,
+        updatedAt: raw.updatedAt
+      })))
+    } catch(e) {
+    console.error(e);
+  } finally {
+    setLoading(false);
+  }
+}
+    fetch();
+  }, []);
+
+  console.log(data)
   
   return (
     <main>
@@ -39,6 +95,31 @@ export const MainPage = () => {
       })}
       getTitle={() => ''}
   />
+        </div>
+        <div className={styles.main__card_list}>
+
+          {loading ? (
+          <Loader size='l' />) : (
+          <div className={styles.main__card_list__text_block}>  
+            <Text view='title' className={styles.main__card_list__title_text}>Total Product</Text>
+            <Text view='p-20' color='accent'>{data ? data.length : ''}</Text>
+            </div>
+          )}
+
+          <div className={styles.main__card_list__grid}>
+          {data.map(data => 
+          <Card
+          className={styles.main__card_list__grid__item}
+          key={data.id} 
+          image={data.images[0]} 
+          captionSlot={data.category.name}
+          title={data.title} 
+          subtitle={data.description}
+          contentSlot={`$${data.price}`}
+          actionSlot={<Button>Add to Cart</Button>}
+          >
+          </Card>)}
+          </div>
         </div>
       </div>
     </main>
