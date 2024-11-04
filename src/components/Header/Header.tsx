f0a920339634f1d022ee3b1bbfcbd34dbf62963b
TakhirovBizhan <bijantakhirov2004@mail.ru>
2024-11-04 07:17:17 +0300

    new: underscore for nav

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,16 +9,38 @@ export const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.header__wrapper}>
-
-      <Link to='/main'><img className={styles.header__logo} src={logoSvg} alt="Логотип" /></Link>
-      <nav className={styles.header__nav}>
-        <NavLink to="/main"><Text view={'p-18'} color='primary' className={styles.header__nav__link}>Products</Text></NavLink>
-        <NavLink to="/Categories"><Text view={'p-18'} color='primary' className={styles.header__nav__link}>Categories</Text></NavLink>
-        <NavLink to="/About_us"><Text view={'p-18'} color='primary' className={styles.header__nav__link}>About us</Text></NavLink>
-      </nav>
-      <div className={styles.header__icons_block}>
-        <img src={bagSvg} alt="корзина" />
-        <img src={userSvg} alt="Профиль" />
+        <Link to='/main'>
+          <img className={styles.header__logo} src={logoSvg} alt="Логотип" />
+        </Link>
+        <nav className={styles.header__nav}>
+          <NavLink 
+            to="/main" 
+            className={({ isActive }) => 
+              isActive ? `${styles.header__nav__link} ${styles.header__nav__link__active}` : styles.header__nav__link
+            }
+          >
+            <Text view='p-18'>Products</Text>
+          </NavLink>
+          <NavLink 
+            to="/Categories" 
+            className={({ isActive }) => 
+              isActive ? `${styles.header__nav__link} ${styles.header__nav__link__active}` : styles.header__nav__link
+            }
+          >
+           <Text view='p-18'> Categories</Text>
+          </NavLink>
+          <NavLink 
+            to="/About_us" 
+            className={({ isActive }) => 
+              isActive ? `${styles.header__nav__link} ${styles.header__nav__link__active}` : styles.header__nav__link
+            }
+          >
+            <Text view='p-18'>About us</Text>
+          </NavLink>
+        </nav>
+        <div className={styles.header__icons_block}>
+          <img src={bagSvg} alt="корзина" />
+          <img src={userSvg} alt="Профиль" />
         </div>
       </div>
     </header>
