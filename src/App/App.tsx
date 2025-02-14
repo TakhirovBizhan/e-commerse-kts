1c7371933466fa9e90e4f98179faa37dabdc80af
TakhirovBizhan <149726252+TakhirovBizhan@users.noreply.github.com>
2025-02-14 23:40:43 +0300

    feat: addded store provider to app, changed my custom hook and created basic store.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,21 +6,25 @@ import { About_us } from './pages/about_us/About_us';
 import ProductPage from './pages/ProductPage';
 import { ROUTES } from '../config/routes';
 import { ThemeProvider } from '../hooks/useThemes/themeProvider';
+import { Provider } from 'react-redux';
+import { store } from '../store';
 
 function App() {
   return (
-    <ThemeProvider>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path={ROUTES.root} element={<MainPage />} />
-          <Route path={ROUTES.product} element={<ProductPage />} />
-          <Route path={ROUTES.categories} element={<Categories />} />
-          <Route path={ROUTES.about_us} element={<About_us />} />
-          <Route path="*" element={<Navigate to="/main" replace />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path={ROUTES.root} element={<MainPage />} />
+            <Route path={ROUTES.product} element={<ProductPage />} />
+            <Route path={ROUTES.categories} element={<Categories />} />
+            <Route path={ROUTES.about_us} element={<About_us />} />
+            <Route path="*" element={<Navigate to="/main" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
--- src/hooks/useProducts/getProducts.ts
+++ src/hooks/useProducts/getProducts.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 import { IData } from "../../config/DataInterfaces";
 import { ErrorHandler } from "../../utils/ErrorHandler";
 
-export const getProducts = async () => {
+export const getProducts = async (offset: number = 0, limit: number = 10) => {
     try {
-        const response = await axios.get<IData[]>('https://api.escuelajs.co/api/v1/products');
+        const response = await axios.get<IData[]>(`https://api.escuelajs.co/api/v1/products?offset=${offset}&limit=${limit}`);
         return response.data;
     } catch (error) {
         const errorMessages = await ErrorHandler.getErrors(error);
