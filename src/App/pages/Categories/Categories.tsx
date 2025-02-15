e63505b5bea4e3c60c501822a921ced1c8410b99
TakhirovBizhan <149726252+TakhirovBizhan@users.noreply.github.com>
2025-02-16 00:15:08 +0300

    feat: created category page

--- src/App/pages/Categories/Categories.tsx
+++ src/App/pages/Categories/Categories.tsx
@@ -1,8 +1,40 @@
+import Button from '../../../components/Button';
+import Card from '../../../components/Card';
+import Loader from '../../../components/Loader';
+import Text from '../../../components/Text';
+import { useGetCategoriesQuery } from '../../../store/api/Categories.api';
+import s from './Categories.module.scss';
 
 export const Categories = () => {
+  const { data, isLoading, error } = useGetCategoriesQuery();
+
   return (
-    <div>Categories</div>
-  )
-}
+    <main>
+      <div className={s.wrapper}>
+        {isLoading ? (
+          <Loader size="l" />
+        ) : data ? (
+          <div className={s.category_list}>
+            {data.map((category) => (
+              <Card
+                className={s.category_card}
+                key={category.id}
+                title={category.name}
+                image={category.image}
+                actionSlot={
+                  <Button className={s.action_btn}>
+                    <Text view="button">See</Text>
+                  </Button>
+                }
+              />
+            ))}
+          </div>
+        ) : error ? (
+          <Text view="p-20">ooops.. Some error, we already trying to fix up.</Text>
+        ) : null}
+      </div>
+    </main>
+  );
+};
 
 export default Categories;
