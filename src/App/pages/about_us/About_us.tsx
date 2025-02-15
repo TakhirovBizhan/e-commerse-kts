4c869286873471c82f7910261f89a11ba30b9bb4
TakhirovBizhan <149726252+TakhirovBizhan@users.noreply.github.com>
2025-02-16 00:32:46 +0300

    feat: created about_us page

--- src/App/pages/about_us/About_us.tsx
+++ src/App/pages/about_us/About_us.tsx
@@ -1,6 +1,10 @@
-
+import Text from '../../../components/Text';
+import s from './About_us.module.scss';
 export const About_us = () => {
   return (
-    <div>About_us</div>
-  )
-}
+    <div className={s.wrapper}>
+      <Text view="title">E-commerse mock service</Text>
+      <Text view="p-20"> What do you call a cow in an earthquake? A milkshake.</Text>
+    </div>
+  );
+};
