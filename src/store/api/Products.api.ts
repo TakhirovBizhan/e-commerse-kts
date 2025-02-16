05298f591ebd4a9547fb0c89ade860f95edc142c
TakhirovBizhan <149726252+TakhirovBizhan@users.noreply.github.com>
2025-02-16 03:30:19 +0300

    feat: now get product have filters and title search

--- src/store/api/Products.api.ts
+++ src/store/api/Products.api.ts
@@ -3,8 +3,18 @@ import { api } from "./api";
 
 export const productsApi = api.injectEndpoints({
     endpoints: builder => ({
-        getProducts: builder.query<IData[], number | void>({
-            query: (page = 0) => `/products?offset=${page}&limit=10`,
+        getProducts: builder.query<IData[], { page?: number, filter?: string, search?: string }>({
+            query: ({ page = 0, filter, search }) => {
+                const urlParams = new URLSearchParams();
+                urlParams.append("offset", page.toString())
+                urlParams.append("limit", '9')
+
+                if (filter) urlParams.append('filter', filter);
+
+                if (search) urlParams.append('title', search);
+
+                return `/products?${urlParams.toString()}`
+            },
             providesTags: ['products']
         }),
         getAllProducts: builder.query<IData[], void>({
