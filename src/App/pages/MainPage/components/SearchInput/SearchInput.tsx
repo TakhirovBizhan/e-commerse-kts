1c8732a43fdd57f4c2ee1ad661ebdd4e271b04f1
TakhirovBizhan <149726252+TakhirovBizhan@users.noreply.github.com>
2025-02-26 01:43:15 +0300

    feat: created hook useDebounce for search input

--- src/App/pages/MainPage/components/SearchInput/SearchInput.tsx
+++ src/App/pages/MainPage/components/SearchInput/SearchInput.tsx
@@ -1,25 +1,34 @@
-import { useState } from "react";
-import Input from "../../../../../components/Input";
-import Button from "../../../../../components/Button";
+import { useEffect, useState } from 'react';
+import Input from '../../../../../components/Input';
+import Button from '../../../../../components/Button';
 import s from './searchInput.module.scss';
+import useDebounce from '../../../../../hooks/UseDebounce';
 
 export const SearchInput = () => {
+  const [inputValue, setInputValue] = useState<string>('');
+  const debouncedSearchTerm = useDebounce(inputValue, 600);
 
-    const [inputValue, setInputValue] = useState<string>('');
+  const handleSearch = () => {
+    console.log('searching for', inputValue);
+  };
 
-    
+  useEffect(() => {
+    if (debouncedSearchTerm) {
+      handleSearch();
+    }
+  }, [debouncedSearchTerm]);
+
   return (
     <div className={s.search_block}>
-            <Input
-    className={s.search_block__input}
-    placeholder='Search product'
-    onChange={(value) => setInputValue(value)}
-    value={inputValue}
-  />
-  <Button>Find now</Button>  
+      <Input
+        className={s.search_block__input}
+        placeholder="Search product"
+        onChange={(value) => setInputValue(value)}
+        value={inputValue}
+      />
+      <Button>Find now</Button>
     </div>
-
-  )
-}
+  );
+};
 
 export default SearchInput;
